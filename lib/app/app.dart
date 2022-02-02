26e476fc84ef8132c6e3aef9f30a951a61b06fb9
surveshoeb <surveshoeb@gmail.com>
2022-02-02 19:09:05 +0530

    Removed unwanted comment

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,3 @@
-// created by Shoeb Surve at 2021/06/19 20:26.
-//
-// Developer email: shoeb@codedaddy.io
-// The code is maintained and managed by Codedaddy Technologies
-// Copyright 2020 The Codedaddy developers. All rights reserved.
-
 import 'package:agenda_boa_test/ui/counter_1/counter_1_viewmodel.dart';
 import 'package:agenda_boa_test/ui/counter_2/counter_2_viewmodel.dart';
 import 'package:agenda_boa_test/ui/counter_3/counter_3_viewmodel.dart';
